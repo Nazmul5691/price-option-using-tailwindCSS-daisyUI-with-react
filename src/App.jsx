ce82be9e97077e9b1916bda4ab986520c64d5567
Nazmul5691 <nazmulislam5691@gmail.com>
2023-09-18 01:31:36 +0600

    added tailwind and daisyUI

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1 className='text-6xl bg-rose-500'>Vite + React</h1>
+      
+
+
+    
+    </>
+  )
+}
+
+export default App
